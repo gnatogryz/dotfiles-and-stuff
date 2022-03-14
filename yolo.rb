e65a6f5f6326784f3db50ddd0fcb4e67b1b26efb
Michał Dobrzański <md@awesome-ind.com>
2022-03-14 14:22:26 +0100

    the yolo script.

---
+++ yolo.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+
+def createLinks(path)
+	Dir.foreach(path) do |d|
+		next if d == '.' || d == '..' || d == '.git' || d == File.basename(__FILE__)
+		if File.directory? d
+			#Dir.chdir(d) do |dir|
+				createLinks( d)
+			#end
+			return
+		end
+		#puts File.join(path, d)
+		home = File.expand_path('~')
+		source = File.join(path, d)
+		target = File.join(home, (File.join(path == '.' ? '' : path, d)))
+
+		puts "Yoloing  #{source} → #{target}"
+		system "rm #{target}" if File.exists? target
+		system "ln #{source} #{target}"
+	end
+end
+
+createLinks '.'

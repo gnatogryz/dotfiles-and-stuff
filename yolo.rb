df65881abd83efe1e6eee04de96ed2bddb91cee9
Michał Dobrzański <plague@go2.pl>
2022-03-14 14:29:45 +0100

    cleanup in #yolo

--- yolo.rb
+++ yolo.rb
@@ -3,13 +3,7 @@
 def createLinks(path)
 	Dir.foreach(path) do |d|
 		next if d == '.' || d == '..' || d == '.git' || d == File.basename(__FILE__)
-		if File.directory? d
-			#Dir.chdir(d) do |dir|
-				createLinks( d)
-			#end
-			return
-		end
-		#puts File.join(path, d)
+		createLinks( d) if File.directory? d
 		home = File.expand_path('~')
 		source = File.join(path, d)
 		target = File.join(home, (File.join(path == '.' ? '' : path, d)))

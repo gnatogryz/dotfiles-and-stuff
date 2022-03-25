095f57fa9c126ed8855c5e3fc0aa72f626f61812
Michał Dobrzański <plague@go2.pl>
2022-03-25 16:58:50 +0100

    feat: check for required applications

--- yolo.rb
+++ yolo.rb
@@ -1,10 +1,29 @@
 #!/usr/bin/env ruby
 
-`which lsd`
-`which ruby`
-`which thefuck`
-`which npm`
-`which autojump`
+class String
+	# colorization
+	def colorize(color_code)
+		"\e[#{color_code}m#{self}\e[0m"
+	end
+
+	def red
+		colorize(31)
+	end
+
+	def green
+		colorize(32)
+	end
+
+	def check
+		puts system("which #{self}", :err => File::NULL, :out => File::NULL) ? "#{self} ✔".green : "#{self} ☒".red 
+	end
+end
+
+['npm', 'lsd', 'thefuck', 'rg', 'autojump'].each do |_|
+	_.check
+end
+
+puts ''
 
 def createLinks(path)
 	Dir.foreach(path) do |d|
